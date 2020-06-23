a193a80d3f364f3150ef20f1127f48ddf82435f0
David Chan <johnsmith20100602@gmail.com>
2020-06-23 23:56:36 +0800

    update tab space settings.

--- Week_01/26_remove_duplicates_from_sorted_array.js
+++ Week_01/26_remove_duplicates_from_sorted_array.js
@@ -4,17 +4,17 @@
  * using JavaScript
  */
 var removeDuplicates = function(nums) {
-    var dupCount = 0;
-    var currentMax = nums[0];
-    var length = nums.length;
-    for (var i = 1; i < length; i++) {
-        if (nums[i] == currentMax) {
-            dupCount += 1;
-        } else {
-            nums[i - dupCount] = nums[i];
-            currentMax = nums[i];
-        }
+  var dupCount = 0;
+  var currentMax = nums[0];
+  var length = nums.length;
+  for (var i = 1; i < length; i++) {
+    if (nums[i] == currentMax) {
+      dupCount += 1;
+    } else {
+      nums[i - dupCount] = nums[i];
+      currentMax = nums[i];
     }
+  }
 
-    return length - dupCount;
+  return length - dupCount;
 };
